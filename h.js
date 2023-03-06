e29471e057bfb29771121ba2a8886944adc042c4
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-06 20:43:04 +0600

    end

---
+++ h.js
@@ -0,0 +1,18 @@
+// const num = 10;
+// num();
+
+// let x = 10;
+// x = y + 5;
+// console.log(x);
+
+// const obj = { name: "John" };
+// Object.keys(obj).forEach((prop) => console.log(prop));
+
+// const name = "John";
+// name.toUpperCase = () => "JOHN";
+// console.log(name.toUpperCase());
+
+// const arr = [1, 2, 3];
+// arr.slice(-2, 0);
+
+const greeting = "Hello, world!";
